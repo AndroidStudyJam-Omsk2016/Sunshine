79da1d743aa65a5ee9f65159e3320f32a7529160
Valdimir N.Yatsuk <yatsukvn@gmail.com>
2016-04-22 22:16:50 +0600

    Add log responce json and change work with githun via token

--- app/src/main/java/loc/developer/vladimiry/sunshine/MainActivityFragment.java
+++ app/src/main/java/loc/developer/vladimiry/sunshine/MainActivityFragment.java
@@ -1,14 +1,23 @@
 package loc.developer.vladimiry.sunshine;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -49,10 +58,87 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
 
+
+//        String json = GetForecastJsonString(
+//                "Omsk",
+//                "json",
+//                "metric",
+//                7,
+//                "6d39fd36fb138d6aca011cd3954a4c97");
+
+
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
 
         return rootView;
     }
+
+    /**
+     * Get data in json format from site openweathermap.org
+     *
+     * @param q     - city name and country code divided by comma, use ISO 3166 country codes
+     * @param mode  - possible values are xml and html. If mode parameter is empty the format is JSON by default
+     * @param units - standard, metric, and imperial units are available
+     * @param cnt   - number of days returned (from 1 to 16)
+     * @param appid - unique application key
+     * @return
+     */
+    public String GetForecastJsonString(String q, String mode, String units, int cnt, String appid) {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        String forecastJsonStr = null;
+
+        try {
+            String url_string = String.format("%s?q=%s&mode=%s&units=%s&cnt=%d&appid=%s",
+                    "http://api.openweathermap.org/data/2.5/forecast/daily", q, mode, units, cnt, appid);
+            URL url = new URL(url_string);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException ex) {
+            Log.e("MainActivityFragment", "Error ", ex);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+
+        return forecastJsonStr;
+    }
+
+
 }
---
+++ app/src/main/java/loc/developer/vladimiry/sunshine/core/OpenWeatherMapParam.java
@@ -0,0 +1,113 @@
+package loc.developer.vladimiry.sunshine.core;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by User on 20.04.2016.
+ */
+public class OpenWeatherMapParam implements Parcelable {
+
+    private String q;
+    private String mode;
+    private String units;
+    private int cnt;
+    private String appid;
+
+    /**
+     *
+     * @param q
+     * @param mode
+     * @param units
+     * @param cnt
+     * @param appid
+     */
+    public OpenWeatherMapParam(String q, String mode, String units, int cnt, String appid)
+    {
+        this.q = q;
+        this.mode = mode;
+        this.units = units;
+        this.cnt = cnt;
+        this.appid = appid;
+    }
+
+    //region Parcelable
+
+    public static final Creator<OpenWeatherMapParam> CREATOR = new Creator<OpenWeatherMapParam>() {
+        public OpenWeatherMapParam createFromParcel(Parcel in) { return new OpenWeatherMapParam(in); }
+        public OpenWeatherMapParam[] newArray(int size) { return new OpenWeatherMapParam[size]; }
+    };
+
+
+    private OpenWeatherMapParam(Parcel in) {
+        readFromParcel(in);
+    }
+
+    private void readFromParcel(Parcel in) {
+        q = in.readString();
+        mode = in.readString();
+        units = in.readString();
+        cnt = in.readInt();
+        appid = in.readString();
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.q);
+        dest.writeString(this.mode);
+        dest.writeString(this.units);
+        dest.writeInt(this.cnt);
+        dest.writeString(this.appid);
+    }
+    //endregion
+
+    //region Getter and Setter
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    //endregion
+}
