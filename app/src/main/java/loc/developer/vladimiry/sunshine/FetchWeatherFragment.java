6fe344601f3ed86db9070a48b89606e2b550f825
Valdimir N.Yatsuk <yatsukvn@gmail.com>
2016-04-24 01:58:16 +0600

    Add item click listener and detail activity

--- app/src/main/java/loc/developer/vladimiry/sunshine/FetchWeatherFragment.java
+++ app/src/main/java/loc/developer/vladimiry/sunshine/FetchWeatherFragment.java
@@ -2,6 +2,7 @@ package loc.developer.vladimiry.sunshine;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
@@ -21,6 +24,7 @@ public class FetchWeatherFragment extends Fragment{
 
     private final String LOG_TAG = FetchWeatherFragment.class.getSimpleName();
     private View mMainView;
+    private ListView listView;
     private ArrayAdapter<String> mForecastAdapter;
     private FetchWeatherTask mTask;
 
@@ -63,6 +67,17 @@ public class FetchWeatherFragment extends Fragment{
                              Bundle savedInstanceState) {
 
         mMainView = inflater.inflate(R.layout.fragment_main, container, false);
+        listView = (ListView) mMainView.findViewById(R.id.listview_forecast);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Snackbar.make(view, forecast, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                // or
+                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_LONG).show();
+            }
+        });
 
         updateView(weekForecast);
 
@@ -76,10 +91,7 @@ public class FetchWeatherFragment extends Fragment{
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
                         data);
-
-        ListView listView = (ListView) mMainView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-
     }
 
     @Override
