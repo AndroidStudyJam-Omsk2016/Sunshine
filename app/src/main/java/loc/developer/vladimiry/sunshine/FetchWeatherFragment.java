0400c827def5a4ca94139a489ed3b9d1a197854a
Valdimir N.Yatsuk <yatsukvn@gmail.com>
2016-04-24 00:06:08 +0600

    Lesson 2 Complete

---
+++ app/src/main/java/loc/developer/vladimiry/sunshine/FetchWeatherFragment.java
@@ -0,0 +1,102 @@
+package loc.developer.vladimiry.sunshine;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FetchWeatherFragment extends Fragment{
+
+    private final String LOG_TAG = FetchWeatherFragment.class.getSimpleName();
+    private View mMainView;
+    private ArrayAdapter<String> mForecastAdapter;
+    private FetchWeatherTask mTask;
+
+    String[] weekForecast = new String[]{
+            "Today - Sunny - 88 / 63",
+            "Tomorrow - Foggy - 70 / 46",
+            "Weds - Cloudy - 72 / 63",
+            "Thurs - Rainy - 64 / 51",
+            "Fri - Foggy - 70 / 46",
+            "Sat - Sunny - 76 / 68"
+    };
+
+    public FetchWeatherFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh) {
+            mTask = new FetchWeatherTask(this);
+            mTask.execute("Omsk");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mMainView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        updateView(weekForecast);
+
+        return mMainView;
+    }
+
+    public void updateView(String[] data) {
+        mForecastAdapter =
+                new ArrayAdapter<>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_forecast, // The name of the layout ID.
+                        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                        data);
+
+        ListView listView = (ListView) mMainView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    public void updateData(String[] data) {
+        if(null != data)
+        {
+            updateView(data);
+        }
+    }
+
+}
--- app/src/main/java/loc/developer/vladimiry/sunshine/MainActivity.java
+++ app/src/main/java/loc/developer/vladimiry/sunshine/MainActivity.java
@@ -1,22 +1,14 @@
 package loc.developer.vladimiry.sunshine;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,12 +24,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FetchWeatherTask weatherTask = new FetchWeatherTask();
-                weatherTask.execute();
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        fab.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -62,83 +53,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
-
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            String q = "Omsk";
-            String mode = "json";
-            String units = "metric";
-            int cnt = 7;
-            String appid = "6d39fd36fb138d6aca011cd3954a4c97";
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                String url_string = String.format("%s?q=%s&mode=%s&units=%s&cnt=%d&appid=%s",
-                        "http://api.openweathermap.org/data/2.5/forecast/daily", q, mode, units, cnt, appid);
-                URL url = new URL(url_string);
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-                Log.i("sunshine", forecastJsonStr);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-            return null;
-        }
-
-
-    }
 }
